30f2ed78db9b0e71f3c8f6403f0b5e38827e14cd
M.Talha Saeed <talhasaeed100@gmail.com>
2022-03-16 21:42:30 +0500

    Validations

--- pract.js
+++ pract.js
@@ -1,2 +1,14 @@
-const mult = (a, b) => console.log(a); console.log(b)
-console.log(mult(2, 3))
+// https://www.youtube.com/watch?v=xRyM33359bY
+const youtubeid = require('get-video-id');
+
+var id = youtubeid("https://www.youtube.com/watch?v=xRyM33359bY");
+
+console.log(id)
+
+function youtube_parser(url){
+    var regExp = /^https?\:\/\/(?:www\.youtube(?:\-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*\&)?vi?=|\&vi?=|\?(?:.*\&)?vi?=)([^#\&\?\n\/<>"']*)/i;
+    var match = url.match(regExp);
+    return (match && match[1].length==11)? match[1] : false;
+}
+
+console.log(youtube_parser("https://www.youtube.com/watchasd?v=xRyM33359bY"))
--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import { useState } from 'react';
 import './App.css';
 import Spinner from './Images/Spinner.gif'
 import youtube from './Images/youtube.png'
+
 function App() {
+  const [error, setError] = useState('false')
   const [loading, setLoading] = useState('false')
   const [cred, setCred] = useState({link:"", title:"" })
-  const [id, setId] = useState(null)
+  const [link, setLink] = useState(null)
   const handleInput = async (e) => {
     setLoading('true');
+    setError('false')
     e.preventDefault();
-    const givenID = id.videoID;
+    const giveLink = link.videoID;
+    
+    var givenID = youtube_parser(giveLink);
+    console.log(givenID)
+    if(givenID === false){
+      setError('true'); 
+    }else{
+      // const givenID = id.videoID;
     const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${givenID}`, {
       "method": "GET",
       "headers": {
@@ -22,17 +32,25 @@ function App() {
     setLoading('false');
     setCred({link:apiResponse.link, title:apiResponse.title})
     document.getElementById('link').style.display = 'flex';
+    }
+
+    
   }
   const onChange = (e) => {
-    setId({ ...id, [e.target.name]: e.target.value })
+    setLink({ ...link, [e.target.name]: e.target.value })
   }
+  function youtube_parser(url){
+    var regExp = /^https?:\/\/(?:www\.youtube(?:-nocookie)?\.com\/|m\.youtube\.com\/|youtube\.com\/)?(?:ytscreeningroom\?vi?=|youtu\.be\/|vi?\/|user\/.+\/u\/\w{1,2}\/|embed\/|watch\?(?:.*&)?vi?=|&vi?=|\?(?:.*&)?vi?=)([^#&?\n<>"']*)/i;
+    var match = url.match(regExp);
+    return (match && match[1].length===11)? match[1] : false;
+}
   return (
     <>
       <div className="top-container">
 
         <form action="" id='form'>
           <h1>YouTube 2 MP3 Converter</h1>
-          <label htmlFor="videoID">Enter Video ID</label> <br />
+          <label htmlFor="videoID">Enter Video Link</label> <br />
           <div >
             <input className='inp' type="text" name="videoID" id="videoID" onChange={onChange} />
             <button type="submit" onClick={handleInput} id='convert-btn'>Submit</button>
@@ -40,7 +58,7 @@ function App() {
         </form>
       </div>
 
-      {loading === 'true'? <img style={{display:'block', margin:'12px auto', width:'300px'}} src={Spinner} alt="Loading" /> :<div style={{display:'none'}} className="bottom-container container" id='link' >
+      {error === 'true'? <div className='errors'> Please Enter Valid YouTube link </div> : (loading === 'true'? <img style={{display:'block', margin:'12px auto', width:'300px'}} src={Spinner} alt="Loading" /> :<div style={{display:'none'}} className="bottom-container container" id='link' >
         <div className="card"  style={{width:'28rem', color:'black', border:'1.5px solid blue', borderRadius:'23px'}} >
           <img src={youtube} style={{width:'26%', display:'block', margin:'auto'}} className="card-img-top" alt="..." />
           <div className="card-body">
@@ -48,7 +66,7 @@ function App() {
             <a href={cred.link} id='download-btn' className="btn btn-primary">Download Here!</a>
           </div>
         </div>
-      </div> }
+      </div>) }
 
     </>
   );
