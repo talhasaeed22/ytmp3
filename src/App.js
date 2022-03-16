d7cf95bdfe5cf4e6d4d7cb53e9ba74bdad514b16
M.Talha Saeed <talhasaeed100@gmail.com>
2022-03-16 12:04:17 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+// import header from './Images/header.jpg'
+import youtube from './Images/youtube.png'
 function App() {
+  const [cred, setCred] = useState({link:"", title:"" })
+  const [id, setId] = useState(null)
+  const handleInput = async (e) => {
+    e.preventDefault();
+    const givenID = id.videoID;
+    console.log(id.videoID)
+    const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${givenID}`, {
+      "method": "GET",
+      "headers": {
+        'x-rapidapi-host': 'youtube-mp36.p.rapidapi.com',
+        'x-rapidapi-key': '0c77206198mshfedeafe1345fc80p19d3ecjsn48dd43210dbb'
+      }
+    });
+
+    const apiResponse = await fetchAPI.json();
+    console.log(apiResponse)
+    setCred({link:apiResponse.link, title:apiResponse.title})
+    document.getElementById('link').style.display = 'flex';
+  }
+  const onChange = (e) => {
+    setId({ ...id, [e.target.name]: e.target.value })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="top-container">
+
+        <form action="" id='form'>
+          <h1>YouTube 2 MP3 Converter</h1>
+          <label htmlFor="videoID">Enter Video ID</label> <br />
+          <div >
+            <input className='inp' type="text" name="videoID" id="videoID" onChange={onChange} />
+            <button type="submit" onClick={handleInput} id='convert-btn'>Submit</button>
+          </div>
+        </form>
+      </div>
+
+      <div className="bottom-container container" id='link' style={{display:'none'}}>
+        <div className="card"  style={{width:'28rem', color:'black', border:'1.5px solid blue', borderRadius:'23px'}} >
+          <img src={youtube} style={{width:'26%', display:'block', margin:'auto'}} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{cred.title}</h5>
+            <a href={cred.link} id='download-btn' className="btn btn-primary">Download Here!</a>
+          </div>
+        </div>
+      </div>
+
+    </>
   );
 }
 
